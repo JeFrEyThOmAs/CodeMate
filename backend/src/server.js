ac01a5e66bd2653a01daa94162b97a97bf531738
Jeffrey Thomas <jeffreythomas10566x@gmail.com>
2025-12-28 14:05:02 +0530

    actual_server for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,21 +40,21 @@ app.get("/video-calls" , protectRoute , (req , res) => {
     console.log(req.user)
 })
 
-if(ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
-    })
-}
-// if (ENV.NODE_ENV === "production") {
-//     const frontendPath = path.join(process.cwd(), "frontend/dist");
-
-//     app.use(express.static(frontendPath));
-
+// if(ENV.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 //     app.get("/{*any}", (req, res) => {
-//         res.sendFile(path.join(frontendPath, "index.html"));
-//     });
+//         res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+//     })
 // }
+if (ENV.NODE_ENV === "production") {
+    const frontendPath = path.join(process.cwd(), "frontend/dist");
+
+    app.use(express.static(frontendPath));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(frontendPath, "index.html"));
+    });
+}
 
 
 const startServer = async() => {

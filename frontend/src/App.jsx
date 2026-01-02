fba857b508967bb97b0a43a949feb6b6b3941606
Jeffrey Thomas <jeffreythomas10566x@gmail.com>
2026-01-02 18:09:14 +0530

    landing-page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,19 @@ import HomePage from './pages/HomePage.jsx'
 import AboutPage from './pages/AboutPage.jsx'
 import ProblemsPage from './pages/ProblemsPage.jsx'
 import {Toaster} from 'react-hot-toast'
+import DashboardPage from './pages/DashboardPage.jsx'
 
 function App() {
-  const{isSignedIn} = useUser();
+  const{isSignedIn , isLoaded} = useUser();
+
+  if(!isLoaded) {
+    return null;
+  }
   return (
     <>
     <Routes>
-      <Route path="/" element={<HomePage/>}></Route>
+      <Route path="/" element={!isSignedIn ? <HomePage/> : <Navigate to="/dashboard"/>}></Route>
+      <Route path="/dashboard" element={isSignedIn ? <DashboardPage/> : <Navigate to="/"/>}></Route>
       <Route path="/problems" element={isSignedIn ? <ProblemsPage/> : <Navigate to="/"/>}></Route>
     </Routes>
     <Toaster toastOptions={{duration : 3000}}/>
